053b8d936e04255241a11574b09c7158dedbcdc8
Gerben van de Weerdhof <gerben1978@hotmail.com>
2023-07-30 13:56:32 +0200

    logo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>
+          Frontend Mentor - REST Countries API with color theme switcher
+        </title>{' '}
+        <link rel="icon" href="/public/favicon.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
